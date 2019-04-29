d53988219b7ca2931157b5a891a9755df70e0826
Vitor Freitas <vitorldfreitas@gmail.com>
2019-04-28 23:34:38 -0300

    change width/height unit to rem and display card faces correctly on screen

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .vue-flipcard(
   :class="[direction, { 'back': back }]"
-  :style="{ 'width': width + 'px', 'height': height + 'px' }"
+  :style="{ 'width': width, 'height': height }"
   @mouseenter="onMouseEnter"
   @mouseleave="onMouseLeave")
   .vue-flipcard__front(:style="{ 'transform-origin': 'center center ' + zoffset + 'px' }")
@@ -12,24 +12,24 @@
 
 <script>
 export default {
-  name: 'VueFlipcard',
-  data () {
+  name: "VueFlipcard",
+  data() {
     return {
       back: false
-    }
+    };
   },
   props: {
     width: {
-      type: Number,
-      default: 300
+      type: String,
+      default: "300px"
     },
     height: {
-      type: Number,
-      default: 300
+      type: String,
+      default: "300px"
     },
     direction: {
       type: String,
-      default: 'horizontal'
+      default: "horizontal"
     },
     zoffset: {
       type: Number,
@@ -41,39 +41,36 @@ export default {
     }
   },
   methods: {
-    flip () {
-      this.back = !this.back
-      this.$emit('flip')
+    flip() {
+      this.back = !this.back;
+      this.$emit("flip");
     },
-    onMouseEnter () {
-      !this.disable && this.flip()
+    onMouseEnter() {
+      !this.disable && this.flip();
     },
-    onMouseLeave () {
-      !this.disable && this.flip()
+    onMouseLeave() {
+      !this.disable && this.flip();
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
 .vue-flipcard {
   position: relative;
-  
+
   &__front,
   &__back {
     width: 100%;
     height: 100%;
     border-radius: 5px;
-    overflow: hidden;
     position: absolute;
-    background-color: #fff;
-    box-shadow: 0 0 15px rgba(173, 173, 173, 0.2);
     perspective: 1000;
     transform-style: preserve-3d;
-    transition: transform .6s;
+    transition: transform 0.6s;
     backface-visibility: hidden; /* backface-visibility 属性指定当元素背面朝向观察者时是否可见。元素的背面总是透明的，当其朝向观察者时，显示正面的镜像 */
   }
-  
+
   &__front {
     z-index: 2;
   }
